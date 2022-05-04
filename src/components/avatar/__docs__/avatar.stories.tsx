f90c112183ff524244d1d8a876d01123321bd429
Mitchell Woodhouse Mckenzie <mitchell.woodhousemckenzie@frontapp.com>
2022-05-04 14:26:09 -0600

    feat: add avatar component (#23)

---
+++ src/components/avatar/__docs__/avatar.stories.tsx
@@ -0,0 +1,25 @@
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import React from 'react';
+
+import {VisualSizesEnum} from '../../../helpers/fontHelpers';
+import {Avatar} from '../avatar';
+
+export default {
+  title: 'Front UI Kit/Avatar',
+  component: Avatar
+} as ComponentMeta<typeof Avatar>;
+
+const Template: ComponentStory<typeof Avatar> = args => <Avatar {...args} />;
+
+export const InitialsAvatar = Template.bind({});
+InitialsAvatar.args = {
+  name: "John Doe"
+};
+
+export const ImageAvatar = Template.bind({});
+ImageAvatar.args = {
+  name: "John Doe",
+  // Example image supplied by: https://picsum.photos/
+  imgSrc: "https://picsum.photos/id/1062/200/200",
+  size: VisualSizesEnum.EXTRA_LARGE
+};
--- src/index.ts
+++ src/index.ts
@@ -14,4 +14,6 @@ export {VisualSizesEnum, fonts} from './helpers/fontHelpers';
  * Components.
  */
 
+export {Avatar} from './components/avatar/avatar';
+
 export {Icon, IconName} from './components/icon/icon';
