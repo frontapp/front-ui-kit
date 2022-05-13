7c4a12c76cfdcc8060491007beed7b00d430a9e3
Mitchell Woodhouse Mckenzie <mitchell.woodhousemckenzie@frontapp.com>
2022-05-13 09:20:19 -0600

    test: add additional tests and required configs

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript', ["@babel/preset-react", {runtime: "automatic"}]],
+  presets: [['@babel/preset-env', {targets: {node: 'current'}}], '@babel/preset-typescript', ["@babel/preset-react", {runtime: "automatic"}]],
   plugins: [
     "add-react-displayname",
     [
--- src/components/button/__tests__/button.spec.tsx
+++ src/components/button/__tests__/button.spec.tsx
@@ -1,63 +1,91 @@
 import {render} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
+import {VisualSizesEnum} from '../../../helpers/fontHelpers';
 import {Icon} from '../../icon/icon';
 import {Button} from '../button';
+import {ButtonGroup} from '../buttonGroup';
 
 describe('<Button /> Spec', () => {
   describe('Snapshots', () => {
     it('should render button using defaults', () => {
-      const component = render(<Button>Button</Button>
-      );
-      expect(component.container).toMatchSnapshot();
+      const view = render(<Button>Button</Button>);
+      expect(view.container).toMatchSnapshot();
     });
 
     it('should render primary button', () => {
-      const component = render(<Button type="primary">Button</Button>
-      );
-      expect(component.container).toMatchSnapshot();
+      const view = render(<Button type="primary">Button</Button>);
+      expect(view.container).toMatchSnapshot();
     });
 
     it('should render primary-danger button', () => {
-      const component = render(<Button type="primary-danger">Button</Button>
-      );
-      expect(component.container).toMatchSnapshot();
+      const view = render(<Button type="primary-danger">Button</Button>);
+      expect(view.container).toMatchSnapshot();
     });
 
     it('should render secondary button', () => {
-      const component = render(<Button type="secondary">Button</Button>
-      );
-      expect(component.container).toMatchSnapshot();
+      const view = render(<Button type="secondary">Button</Button>);
+      expect(view.container).toMatchSnapshot();
     });
 
     it('should render secondary-danger button', () => {
-      const component = render(<Button type="secondary-danger">Button</Button>
-      );
-      expect(component.container).toMatchSnapshot();
+      const view = render(<Button type="secondary-danger">Button</Button>);
+      expect(view.container).toMatchSnapshot();
     });
 
     it('should render tertiary button', () => {
-      const component = render(<Button type="tertiary">Button</Button>
+      const view = render(<Button type="tertiary">Button</Button>);
+      expect(view.container).toMatchSnapshot();
+    });
+
+    it('should render different sized button', () => {
+      const view = render(
+        <ButtonGroup>
+          <Button size={VisualSizesEnum.LARGE}>Button</Button>
+          <Button size={VisualSizesEnum.MEDIUM}>Button</Button>
+          <Button size={VisualSizesEnum.SMALL}>Button</Button>
+        </ButtonGroup>
       );
-      expect(component.container).toMatchSnapshot();
+      expect(view.container).toMatchSnapshot();
     });
 
     it('should render icon button', () => {
-      const component = render(
+      const view = render(
         <Button type="icon">
           <Icon name="Close" />
         </Button>
       );
-      expect(component.container).toMatchSnapshot();
+      expect(view.container).toMatchSnapshot();
     });
 
     it('should render icon-danger button', () => {
-      const component = render(
+      const view = render(
         <Button type="icon-danger">
           <Icon name="Close" />
         </Button>
       );
-      expect(component.container).toMatchSnapshot();
+      expect(view.container).toMatchSnapshot();
     });
+  });
+
+  it('should issue onClick events when clicked', async () => {
+    const user = userEvent.setup();
+    const onClickMock = jest.fn();
+    const view = render(<Button onClick={onClickMock}>Button</Button>);
+
+    await user.click(view.getByRole('button', {name: /Button/i}));
+
+    expect(onClickMock).toBeCalled();
+  });
+
+  it('should not issue onClick events when clicked when isDisabled', async () => {
+    const user = userEvent.setup();
+    const onClickMock = jest.fn();
+    const view = render(<Button onClick={onClickMock} isDisabled>Button</Button>);
+
+    await user.click(view.getByRole('button', {name: /Button/i}));
+
+    expect(onClickMock).not.toBeCalled();
   });
 });
