9da9099f7180e27e05726a05ec8072c1d8a83644
Mitchell Woodhouse Mckenzie <mitchell.woodhousemckenzie@frontapp.com>
2022-05-24 10:30:30 -0600

    feat: add dropdownItemAvatar component (#65)
    feat: add hasAvatar to dropdownItemSkeleton

---
+++ src/components/dropdown/dropdownItemAvatar.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, {FC} from 'react';
+import styled from 'styled-components';
+
+import {VisualSizesEnum} from '../../helpers/fontHelpers';
+import {Avatar, AvatarProps} from '../avatar/avatar';
+
+/*
+ * Style.
+ */
+
+const StyledDropdownAvatarWrapperDiv = styled.div`
+  grid-area: left-content;
+  margin-right: 12px;
+`;
+
+/*
+ * Component.
+ */
+
+export const DropdownItemAvatar: FC<AvatarProps> = props => (
+  <StyledDropdownAvatarWrapperDiv>
+    <Avatar {...props} size={props.size || VisualSizesEnum.SMALL} />
+  </StyledDropdownAvatarWrapperDiv>
+);
