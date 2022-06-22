8306affcc6dd1c0bd260c5520bf81d310b87b8b5
Mitchell Woodhouse Mckenzie <mitchell.woodhousemckenzie@frontapp.com>
2022-06-22 14:22:23 -0600

    docs: update checkbox docs (#134)
    * docs: update checkbox docs
    
    feat: clicking checkbox label will fire the onchange
    
    * chore: update snapshots for checkbox
    
    * chore: minor style change

---
+++ src/components/checkbox/__docs__/index.stories.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable storybook/story-exports */
+import {ComponentMeta} from '@storybook/react';
+
+import {Checkbox} from '../checkbox';
+import DocumentationMDX from './docs.mdx';
+
+/*
+ * Storybook.
+ */
+
+export default {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  parameters: {
+    docs: {
+      page: DocumentationMDX
+    },
+    previewTabs: {
+      canvas: {
+        hidden: true
+      }
+    },
+    viewMode: 'docs'
+  },
+  id: "Components/Checkbox"
+} as ComponentMeta<typeof Checkbox>;
+
+export {Basic} from './stories/basic.stories';
+export {Indeterminate} from './stories/indeterminate.stories';
