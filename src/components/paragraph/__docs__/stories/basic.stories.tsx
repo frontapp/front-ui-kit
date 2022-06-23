51d842c960866b611e53f4a3c4dedf84de5683be
Mitchell Woodhouse Mckenzie <mitchell.woodhousemckenzie@frontapp.com>
2022-06-23 14:15:25 -0600

    chore: use lorum ipsum

--- src/components/paragraph/__docs__/stories/basic.stories.tsx
+++ src/components/paragraph/__docs__/stories/basic.stories.tsx
@@ -12,7 +12,14 @@ const StyledCenteredDiv = styled.div`
 
 const Template: ComponentStory<typeof Paragraph> = () => (
   <StyledCenteredDiv>
-    <Paragraph>Example Paragraph</Paragraph>
+    <Paragraph>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at mattis lorem, id varius sem.
+      Praesent commodo eu ex sit amet cursus. Sed condimentum tortor urna, ut dapibus odio vehicula a. Quisque
+      nec odio lorem. In hac habitasse platea dictumst. Mauris dictum varius ultrices. Morbi sed lacus auctor,
+      blandit enim ac, dignissim libero. Praesent a massa augue. Sed turpis dui, accumsan sed tristique in,
+      tincidunt nec metus. In nec vehicula est. Maecenas venenatis tincidunt congue. Ut at fermentum urna. Sed
+      vestibulum mattis dapibus.
+    </Paragraph>
   </StyledCenteredDiv>
 );
 
