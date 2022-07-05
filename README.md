0ea9a173da1da20af06fd85474eb52d64731c8a4
Mitchell Woodhouse Mckenzie <mitchell.woodhousemckenzie@frontapp.com>
2022-07-05 10:02:16 -0600

    feat: add async search story
    feat: update README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Front UI Kit - 🚧 Under Construction 🚧
+# Front UI Kit - Beta
 
 <p align="center">
   <a href="https://badge.fury.io/js/@frontapp%2Fui-kit">
@@ -8,3 +8,25 @@
     <img src="https://cdn.jsdelivr.net/gh/storybookjs/brand@main/badge/badge-storybook.svg" alt="Storybook" />
   </a>
 </p>
+
+## Installation
+
+To install you would just need to install via npm:
+
+```
+npm install @frontapp/ui-kit
+```
+
+or yarn:
+
+```
+yarn add @frontapp/ui-kit
+```
+
+## Documentation
+
+You can view the source code for the different components in this repo but for full documentation view the developer portal https://dev.frontapp.com/docs (Documentation is a work in progress).
+
+## Bugs or issues?
+
+Feel free to open up an issue if you run into any problems using the library.
---
+++ src/components/_pre-built/select/__docs__/stories/async-search.stories.tsx
@@ -0,0 +1,92 @@
+import {ComponentStory} from '@storybook/react';
+import React, {useEffect, useState} from 'react';
+import styled from 'styled-components';
+
+import {Select} from '../../select';
+import {SelectItem} from '../../selectItem';
+
+interface UserData {
+  id: string;
+  name: string;
+}
+
+const StyledWrapperDiv = styled.div`
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+  height: 450px;
+`;
+
+const StyledMenuWrapperDiv = styled.div`
+  padding: 8px;
+  border-radius: 8px;
+  background: white;
+  width: 350px;
+`;
+
+const Template: ComponentStory<typeof Select> = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [users, setUsers] = useState<ReadonlyArray<UserData>>([]);
+  const [usersToDisplay, setUsersToDisplay] = useState<ReadonlyArray<UserData>>([]);
+  const [selectedUserId, setSelectedUserId] = useState<string>();
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    if (!searchValue) {
+      setUsersToDisplay(users);
+      return;
+    }
+
+    setUsersToDisplay(users.filter(u => u.name.toLowerCase().includes(searchValue.toLowerCase())));
+  }, [searchValue, users]);
+
+  const fetchUserData = async () => {
+    if (isLoading)
+      return;
+    setIsLoading(true);
+    // Create a 1 second delay between requests.
+    setTimeout(async () => {
+      const data = await fetch(`https://randomuser.me/api/?results=50&seed=example`);
+      const jsonData = await data.json();
+      setUsers(existingUsers => [
+        ...existingUsers,
+        ...jsonData.results.map(d => ({id: d.login.uuid, name: `${d.name.first} ${d.name.last}`}))
+      ]);
+      setIsLoading(false);
+    }, 1000);
+  };
+
+  return (
+    <StyledWrapperDiv>
+      <StyledMenuWrapperDiv>
+        <Select
+          selectedValues={users.find(user => user.id === selectedUserId)?.name}
+          isLoading={isLoading}
+          hasMore={users.length === 0}
+          onLoadMore={fetchUserData}
+          headerLabel="Search for a user"
+          searchValue={searchValue}
+          onSearchChange={value => {
+            setSearchValue(value);
+          }}
+          onSelectClosed={() => {
+            setSearchValue('');
+          }}
+          layerRootId="story--components-select--async-search"
+        >
+          {usersToDisplay.map(user => (
+            <SelectItem
+              key={user.id}
+              onClick={() => setSelectedUserId(user.id)}
+              isSelected={user.id === selectedUserId}
+            >
+              {user.name}
+            </SelectItem>
+          ))}
+        </Select>
+      </StyledMenuWrapperDiv>
+    </StyledWrapperDiv>
+  );
+};
+
+export const AsyncSearch = Template.bind({});
