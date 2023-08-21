932b820e4559291bfbf6862587ae87cba29c9941
Damien Erambert <damien@erambert.me>
2023-08-21 15:11:40 -0700

    :shirt:

--- src/components/loader/__docs__/stories/basic.stories.tsx
+++ src/components/loader/__docs__/stories/basic.stories.tsx
@@ -4,14 +4,14 @@ import React from 'react';
 import {VisualSizesEnum} from '../../../../helpers/fontHelpers';
 import {Loader, LoaderColorVariantsEnum} from '../../loader';
 
-export const Basic: ComponentStory<typeof Loader> = (args) => (
+export const Basic: ComponentStory<typeof Loader> = args => (
   <div style={{display: 'flex', gap: 10}}>
     <Loader {...args} size={VisualSizesEnum.SMALL} color={LoaderColorVariantsEnum.BLUE} />
     <Loader {...args} size={VisualSizesEnum.SMALL} color={LoaderColorVariantsEnum.GREY} />
   </div>
 );
 
-export const Big: ComponentStory<typeof Loader> = (args) => (
+export const Big: ComponentStory<typeof Loader> = args => (
   <div style={{display: 'flex', gap: 20}}>
     <Loader {...args} size={VisualSizesEnum.LARGE} color={LoaderColorVariantsEnum.BLUE} />
     <Loader {...args} size={VisualSizesEnum.LARGE} color={LoaderColorVariantsEnum.GREY} />
