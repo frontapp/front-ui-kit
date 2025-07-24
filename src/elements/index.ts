85d51f9002608f8971175debe45ba2e288788c18
Daniel Darritchon <daniel.darritchon@front.com>
2025-07-24 14:38:41 -0400

    fixed export not found errors

--- src/elements/index.ts
+++ src/elements/index.ts
@@ -1,20 +1,21 @@
-export {DropdownCoordinator} from './dropdown/dropdownCoordinator';
-export {Dropdown} from './dropdown/dropdown';
-export {DropdownButton} from './dropdown/dropdownButton';
-export {DropdownHeader} from './dropdown/dropdownHeader';
-export {DropdownHeading} from './dropdown/dropdownHeading';
-export {DropdownItem} from './dropdown/dropdownItem';
-export {DropdownItemAvatar} from './dropdown/dropdownItemAvatar';
-export {DropdownItemIcon} from './dropdown/dropdownItemIcon';
-export {DropdownItemFormField} from './dropdown/dropdownItemFormField';
-export {DropdownFooter} from './dropdown/dropdownFooter';
-export {DropdownItemSkeleton} from './dropdown/skeleton/dropdownItemSkeleton';
-export {DropdownItemSpacer} from './dropdown/dropdownItemSpacer';
+export { DropdownCoordinator } from './dropdown/dropdownCoordinator';
+export { Dropdown } from './dropdown/dropdown';
+export { DropdownButton } from './dropdown/dropdownButton';
+export { DropdownHeader } from './dropdown/dropdownHeader';
+export { DropdownHeading } from './dropdown/dropdownHeading';
+export { DropdownItem } from './dropdown/dropdownItem';
+export { DropdownItemAvatar } from './dropdown/dropdownItemAvatar';
+export { DropdownItemIcon } from './dropdown/dropdownItemIcon';
+export { DropdownItemFormField } from './dropdown/dropdownItemFormField';
+export { DropdownFooter } from './dropdown/dropdownFooter';
+export { DropdownItemSkeleton } from './dropdown/skeleton/dropdownItemSkeleton';
+export { DropdownItemSpacer } from './dropdown/dropdownItemSpacer';
 
-export {EmptyState} from './emptyState/emptyState';
+export { EmptyState } from './emptyState/emptyState';
 
-export {Icon, IconName} from './icon/icon';
+export { Icon } from './icon/icon';
+export type { IconName } from './icon/icon';
 
-export {Input} from './input/input';
+export { Input } from './input/input';
 
-export {Textarea} from './textarea/textarea';
+export { Textarea } from './textarea/textarea';
--- src/index.ts
+++ src/index.ts
@@ -2,84 +2,87 @@
  * Interfaces.
  */
 
-export {SelectableComponentColors} from './helpers/styleHelpers';
+export type { SelectableComponentColors } from './helpers/styleHelpers';
 
 /*
  * Constants.
  */
 
-export {VisualSizesEnum, fonts} from './helpers/fontHelpers';
-export {PaletteColorsEnum} from './helpers/colorHelpers';
+export { VisualSizesEnum, fonts } from './helpers/fontHelpers';
+export { PaletteColorsEnum } from './helpers/colorHelpers';
 
 /*
  * Helpers.
  */
 
-export {useTimeout} from './helpers/hookHelpers';
+export { useTimeout } from './helpers/hookHelpers';
 
 /*
  * Utils.
  */
 
-export {DefaultStyleProvider} from './utils/defaultStyleProvider';
+export { DefaultStyleProvider } from './utils/defaultStyleProvider';
 
 /*
  * Components.
  */
 
-export {Accordion} from './components/accordion/accordion';
-export {AccordionSection} from './components/accordion/accordionSection';
+export { Accordion } from './components/accordion/accordion';
+export { AccordionSection } from './components/accordion/accordionSection';
 
-export {ActionMenu} from './components/_pre-built/actionMenu/actionMenu';
-export {ActionMenuItem} from './components/_pre-built/actionMenu/actionMenuItem';
-export {ActionMenuItemSpacer} from './components/_pre-built/actionMenu/actionMenuItemSpacer';
+export { ActionMenu } from './components/_pre-built/actionMenu/actionMenu';
+export { ActionMenuItem } from './components/_pre-built/actionMenu/actionMenuItem';
+export { ActionMenuItemSpacer } from './components/_pre-built/actionMenu/actionMenuItemSpacer';
 
-export {Avatar} from './components/avatar/avatar';
+export { Avatar } from './components/avatar/avatar';
 
-export {Button, ButtonTypes} from './components/button/button';
-export {ButtonContent} from './components/button/buttonContent';
-export {ButtonContentIcon} from './components/button/buttonContentIcon';
-export {ButtonGroup} from './components/button/buttonGroup';
+export { Button } from './components/button/button';
+export type { ButtonTypes } from './components/button/button';
+export { ButtonContent } from './components/button/buttonContent';
+export { ButtonContentIcon } from './components/button/buttonContentIcon';
+export { ButtonGroup } from './components/button/buttonGroup';
 
-export {Checkbox} from './components/checkbox/checkbox';
+export { Checkbox } from './components/checkbox/checkbox';
 
-export {DatePickerDropdown as DatePicker} from './components/datepicker/datepickerDropdown';
- 
-export {File, FileTypesEnum} from './components/_pre-built/file/file';
+export { DatePickerDropdown as DatePicker } from './components/datepicker/datepickerDropdown';
 
-export {InlineBanner} from './components/inlineBanner/inlineBanner';
+export { File, FileTypesEnum } from './components/_pre-built/file/file';
 
-export {FormField} from './components/formField/formField';
+export { InlineBanner } from './components/inlineBanner/inlineBanner';
 
-export {Pill} from './components/pill/pill';
-export {PillContent} from './components/pill/pillContent';
-export {PillContentIcon} from './components/pill/pillContentIcon';
+export { FormField } from './components/formField/formField';
 
-export {Select} from './components/_pre-built/select/select';
-export {SelectItem} from './components/_pre-built/select/selectItem';
+export { Pill } from './components/pill/pill';
+export { PillContent } from './components/pill/pillContent';
+export { PillContentIcon } from './components/pill/pillContentIcon';
 
-export {Skeleton} from './components/skeleton/skeleton';
+export { Select } from './components/_pre-built/select/select';
+export { SelectItem } from './components/_pre-built/select/selectItem';
 
-export {Tab} from './components/tab/tab';
-export {TabGroup} from './components/tab/tabGroup';
+export { Skeleton } from './components/skeleton/skeleton';
 
-export {Task} from './components/_pre-built/task/task';
+export { Tab } from './components/tab/tab';
+export { TabGroup } from './components/tab/tabGroup';
 
-export {Tooltip} from './components/tooltip/tooltip';
-export {TooltipCoordinator} from './components/tooltip/tooltipCoordinator';
+export { Task } from './components/_pre-built/task/task';
 
-export {TopBanner} from './components/topBanners/topBanner';
-export {TopBannersHost} from './components/topBanners/topBannersHost';
-export {TopBannersProvider} from './components/topBanners/topBannersProvider';
+export { Tooltip } from './components/tooltip/tooltip';
+export { TooltipCoordinator } from './components/tooltip/tooltipCoordinator';
+
+export { TopBanner } from './components/topBanners/topBanner';
+export { TopBannersHost } from './components/topBanners/topBannersHost';
+export { TopBannersProvider } from './components/topBanners/topBannersProvider';
 export {
   TopBannerContext,
   TopBannersEnum,
-  TopBannerDefinition,
-  TopBannerContextProps,
   useTopBanners
 } from './components/topBanners/topBannersContext';
+export type {
+  TopBannerDefinition,
+  TopBannerContextProps
+} from './components/topBanners/topBannersContext';
 
-export {Loader} from './components/loader/loader';
+export { Loader } from './components/loader/loader';
 
 /*
  * Additional Exports.
