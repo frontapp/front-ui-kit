e6e0080ee014422f9c50dd5c92184ba33485ab93
Daniel Darritchon <daniel.darritchon@front.com>
2025-08-11 11:45:36 -0400

    removed uneeded changes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,5 @@
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
-  docs: {
-    source: {
-      type: 'code',
-    },
-  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
